5edd4185bb2b79d70845ef1ff8ffd76416b7c8ab
aowuor <angelaowuor@gmail.com>
2023-02-28 07:38:20 +0300

    commit tree 9e39c3a65dfeb437f5ada16cf972726e008abbdc

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,11 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    something = self.name + ' always says:'
+    thing_said = something + " "+ self.catchphrase
+    thing_said
+  end
+
 end
